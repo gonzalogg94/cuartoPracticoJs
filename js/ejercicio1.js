ceef4606f83a3913c00efa77ff4f7956a2ac7245
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-07-25 13:55:04 -0300

    realice el ejercicio numero 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,12 @@
+/*
+1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
+*/
+
+let auto0={
+    color:"gris",
+    marca:"ford",
+    modelo:"ka",
+    año:2017,
+    encender:()=>{document.write("<p>El dispositivo se apago</p>")},
+    apagar:()=>{document.write("<p>El dispositivo se apago</p>")}
+}
